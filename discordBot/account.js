e56645a378a0201397ea2d522a9a420753448118
koldin108902 <alex108902@naver.com>
2022-03-01 05:16:25 +0900

    유저가 아닌 것을 멘션했을때 예외처리

--- discordBot/account.js
+++ discordBot/account.js
@@ -92,12 +92,13 @@ function numberToKorean(number){
 }
 
 function donate(cmd, msg) {
-    const mention = msg.mentions.users.values().next().value.id
+    const mention = msg.mentions.users.values().next().value.id || undefined
     const price = Number(cmd[2]) || ""
     const commission = Math.round(price * 0.07)
     let users = JSON.parse(fs.readFileSync("./data/user.json", 'utf-8'))
 
     if (typeof price !== "number" || price < 10000 || price % 1 !== 0) return msg.channel.send("송금액을 재확인 해주십시오. ex) #송금 유저멘션 송금액(최소 만원)")
+    if (mention === undefined) return msg.channel.send("멘션을 재확인 해주십시오.")
     
     for (let i = 0; i < users.length; i++) {
         if (users[i].userId === msg.author.id) {
