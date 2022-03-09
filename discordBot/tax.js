4285962c219dae10008e4b1b87e9802b7544ce0d
koldin108902 <alex108902@naver.com>
2022-03-10 03:03:40 +0900

    update: 유저의 보유금이 10억 이하면 세금 초기화

--- discordBot/tax.js
+++ discordBot/tax.js
@@ -4,7 +4,10 @@ function CollectionTax() {
     const users = JSON.parse(fs.readFileSync("./data/user.json", 'utf-8'))
     
     for (let i = 0; i < users.length; i++) {
-        if (users[i].coin < 1000000000) continue
+        if (users[i].coin < 1000000000) {
+            users[i].tax = 0
+            continue
+        }
         users[i].tax = users[i].coin / 100000
 
         // 10억 이상의 금액을 소유하고있으면 0.001%의 금액을 세금으로서 징수
