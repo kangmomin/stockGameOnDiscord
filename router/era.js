a3d86e928f2b8c9575d3fb1c3baa05b671afbde7
koldin108902 <alex108902@naver.com>
2022-06-18 07:44:12 +0900

    fixed: era 조건식

--- router/era.js
+++ router/era.js
@@ -12,7 +12,7 @@ function era(cmd, msg) {
     }
 
     if (idx === null) return msg.channel.send("[#가입]을 먼저 진행해 주십시오.")
-    if (users[idx].coin < 100 && users[idx].bank < 0 && users[idx].era > 0) {
+    if (users[idx].coin < 100 && users[idx].bank < 1 && users[idx].era > 0) {
         for (let i = 0; i < users[idx].stock.length; i++) {
             if (users[idx].stock[i] > 0) return msg.channel.send("긴급구제신청이 불가합니다.")
         }
